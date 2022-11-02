c3c452c9831c2f444603c7603340acb8a05f406a
Anna Harder <annaqharder@gmail.com>
2022-11-02 11:22:34 -0600

    commit tree 64d082af5a4e3eb4b79bd42444c0d23d25d0f49e

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Your code goes here
+// document.querySelector('text').addEventListener('DOMContentLoaded', function() {
+//     console.log("This is really cool!")
+// })
+
+// p.textContent = 
+
+
+// document.querySelector('text').textContent = 'This is really cool!'
+
+document.addEventListener("DOMContentLoaded", function() { 
+   document.querySelector('#text').textContent = "This is really cool!";  
+}); 
+    
+
+
+// document.addEventListener("DOMContentLoaded", cb() { 
+//     cb() {
+//         document.querySelector('#text').textContent = "This is so cool!"; 
+//     }
+// })
+
